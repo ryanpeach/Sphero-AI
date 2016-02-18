f3f6ee7daa57f4bf8fd903a50769f7744fd6db8f
Ryan Peach <ryan.peach@outlook.com>
2016-02-18 00:17:57 +0000

    Recieved remote connection

--- Remote.py
+++ Remote.py
@@ -1,12 +1,12 @@
 #http://www.tutorialspoint.com/python/python_networking.htm
 
 import socket               # Import socket module
+import sphero
 
 s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
+host = ''                   # Get local machine name
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
-print(s)
 s.listen(5)                 # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
