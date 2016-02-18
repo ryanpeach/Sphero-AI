3464ec2ce1a4e5f5abdd239558d9781d659a8b50
Ryan Peach <ryan.peach@outlook.com>
2016-02-17 21:31:08 -0600

    Working on remote

--- Remote.py
+++ Remote.py
@@ -1,7 +1,7 @@
 #http://www.tutorialspoint.com/python/python_networking.htm
 
 import socket               # Import socket module
-import sphero
+from sphero import *
 
 # Initialize Sphero
 robot = core.Sphero("/dev/rfcomm0")
@@ -14,7 +14,11 @@ port = 12345                # Reserve a port for your service.
 net.bind((host, port))        # Bind to the port
 
 # Initialize Commands
-Commands = {"get_rgb": lambda x,y: robot.get_rgb()}
+Commands = {"get_rgb"    : lambda x,y,z: robot.get_rgb(),
+            "set_rgb"    : lambda x,y,z: robot.set_rgb(int(x),int(y),int(z),True),
+            "ping"       : lambda x,y,z: robot.ping(),
+            "set_heading": lambda x,y,z: robot.set_heading(int(x)),
+            "set_speed"  : lambda x,y,z: robot.set_rotation_rate(int(x))}
 
 net.listen(5)                 # Now wait for client connection.
 while True:                   # Runs forever
@@ -26,10 +30,12 @@ while True:                   # Runs forever
     while True:                # Runs until broken by error
        try:
            cmd = s.recv(1024)   # Receive the command
-           p1  = s.recv(1024)   # Receive p1
-           p2  = s.recv(1024)   # Receive p2
+           v1  = s.recv(1024)   # Receive v1
+           v2  = s.recv(1024)   # Receive v2
+           v3  = s.recv(1024)   # Receive v3
 
-           Commands[cmd](p1,p2)
+           Commands[cmd](v1,v2,v3)
+           c.send("Done!")
 
        except:
            break
