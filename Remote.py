175a1e349eb916d928d8d92932214b88c75adcc8
Ryan Peach <ryan.peach@outlook.com>
2016-02-18 01:37:42 +0000

    More remote changes

--- Remote.py
+++ Remote.py
@@ -7,6 +7,7 @@ s = socket.socket()         # Create a socket object
 host = ''                   # Get local machine name
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
+print(s)
 s.listen(5)                 # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
