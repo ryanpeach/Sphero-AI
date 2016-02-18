408378f9e78ecbe51981f9aee10d123a84a35b0d
Ryan Peach <ryan.peach@outlook.com>
2016-02-17 19:53:31 -0600

    Working on controlling sphero.

--- Remote.py
+++ Remote.py
@@ -3,14 +3,35 @@
 import socket               # Import socket module
 import sphero
 
-s = socket.socket()         # Create a socket object
+# Initialize Sphero
+robot = core.Sphero("/dev/rfcomm0")
+robot.connect()
+
+# Initialize Socket
+net = socket.socket()         # Create a socket object
 host = ''                   # Get local machine name
 port = 12345                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
-print(s)
-s.listen(5)                 # Now wait for client connection.
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print 'Got connection from', addr
-   c.send('Thank you for connecting')
-   c.close()                # Close the connection
+net.bind((host, port))        # Bind to the port
+
+# Initialize Commands
+Commands = {"get_rgb": lambda x,y: robot.get_rgb()}
+
+net.listen(5)                 # Now wait for client connection.
+while True:                   # Runs forever
+    # Establish connection with client.
+    c, addr = net.accept()
+    print 'Got connection from', addr
+    c.send('Thank you for connecting')
+
+    while True:                # Runs until broken by error
+       try:
+           cmd = s.recv(1024)   # Receive the command
+           p1  = s.recv(1024)   # Receive p1
+           p2  = s.recv(1024)   # Receive p2
+
+           Commands[cmd](p1,p2)
+
+       except:
+           break
+
+    c.close()                # Close the connection
